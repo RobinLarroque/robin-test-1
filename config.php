cbefa1e629da9f510e831ecfa8b741c3f5f17bc9
Robin Larroque <robin.larroque@axa-tech.com>
2016-02-09 17:44:22 +0100

    robin-test init

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$db_name= getenv ('MYSQL_DB');
+
+$db_host= getenv ('MYSQL_HOST');
+
+$db_pass= getenv ('MYSQL_PASS');
+
+$db_port= getenv ('MYSQL_PORT');
+
+$db_user= getenv ('MYSQL_USER');
+
+$file = '/data/sharedfs/test.txt';
+$current = "Text included into the test file-20151127";
+?>
